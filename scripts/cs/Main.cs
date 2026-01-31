1e0b270053d356f4a9dfa2fd2e6d9cba9557bc44
sackofcookies <tommy.lesueur.temp@outlook.com>
2026-01-30 19:48:13 -0700

    camera

--- scripts/cs/Main.cs
+++ scripts/cs/Main.cs
@@ -1,19 +1,31 @@
-using Godot;
 using System;
+using System.ComponentModel.DataAnnotations;
+using Godot;
 
 public partial class Main : Node2D
 {
 
 	private Camera2D camera;
+	private CharacterBody2D player;
+	[Export]
+	public float LockVelocity {get; set;} = 2500F;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		this.camera = GetNode<Camera2D>("Camera");
+		this.player = GetNode<CharacterBody2D>("Player");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		camera.Position = new Vector2(camera.Position.X, player.Position.Y);
+		if (player.Velocity.Y > LockVelocity){
+			camera.PositionSmoothingEnabled = false;
+		}
+		else {
+			camera.PositionSmoothingEnabled = true;
+		}
 
 	}
 }
